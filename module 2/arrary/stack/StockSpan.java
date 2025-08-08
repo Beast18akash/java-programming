1458ae11fc118b41ecfd7cecf3438c8b26dc3b8a
Beast18akash <holsambreakash18@gmail.com>
2025-08-08 20:43:46 +0530

    nextcode

---
+++ module 2/arrary/stack/StockSpan.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class StockSpan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        Stack<Integer> stack = new Stack<>();
+        int[] span = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] <= arr[i]) {
+                stack.pop();
+            }
+            span[i] = stack.isEmpty() ? i + 1 : i - stack.peek();
+            stack.push(i);
+        }
+        
+        for (int s : span) {
+            System.out.print(s + " ");
+        }
+    }
+}
