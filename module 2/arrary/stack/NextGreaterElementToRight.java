483671a6d5ca5d4d6a73298948a19614e96594dd
Beast18akash <holsambreakash18@gmail.com>
2025-08-08 20:23:38 +0530

    next program done

---
+++ module 2/arrary/stack/NextGreaterElementToRight.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class NextGreaterElementToRight {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[n];
+        
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+            result[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(arr[i]);
+        }
+        
+        for (int res : result) {
+            System.out.print(res + " ");
+        }
+    }
+}
